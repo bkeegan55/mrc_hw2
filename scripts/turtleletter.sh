f5bd293d9100563c1f5069822bf16bc9811ee7d3
Ben Keegan <bpkeegan@nps.edu>
2018-04-11 11:14:56 -0700

    fourth commit

---
+++ scripts/turtleletter.sh
@@ -0,0 +1,3 @@
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, -4.5]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[2.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, -4.5]'
